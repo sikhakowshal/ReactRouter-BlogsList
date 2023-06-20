00a8198e2ac1d8ca24743d776a8ba87ff41b85bd
Sikha Kowshal <koushalsikha4@gmail.com>
2023-06-20 11:55:31 +0000

    final-code

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const BlogItem = props => {
+  const {eachBlog} = props
+  const {title, description, publishedDate} = eachBlog
+
+  return (
+    <li className="blog-item">
+      <div className="title-container">
+        <h1 className="title">{title}</h1>
+        <p className="date">{publishedDate}</p>
+      </div>
+      <p className="description">{description}</p>
+    </li>
+  )
+}
+
+export default BlogItem
---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="profile-img"
+    />
+    <h1 className="name">Wade Warren</h1>
+    <p className="job-description">Software developer at UK</p>
+  </div>
+)
+
+export default UserInfo
